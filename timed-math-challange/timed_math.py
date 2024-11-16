0cdef0984097bc4321f565d848d54f096a823d08
Ahmet Hakan Yıldız <129862074+yildizahmethakan0@users.noreply.github.com>
2024-11-16 21:51:44 +0300

    Update timed_math.py

--- timed-math-challange/timed_math.py
+++ timed-math-challange/timed_math.py
@@ -6,7 +6,6 @@ MIN_OPERAND = 3
 MAX_OPERAND = 12
 TOTAL_PROBLEMS = 10
 
-
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
     right = random.randint(MIN_OPERAND, MAX_OPERAND)
